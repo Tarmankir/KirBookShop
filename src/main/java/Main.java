6832b021a3a8814eca6b50f460b090cc15b48091
aleksey_krapivnitskiy <krapivnitskiy@secureducation.ru>
2021-03-15 18:37:24 +0300

    add todos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,19 @@
 public class Main extends BasisOfCheck {
 
+        //todo add at least inner class
+        //todo use collections
+
         public static void main(String[] args) {
-        Book programmingbook = new Book();
-        programmingbook.setName("Лучший учебник по Джава ");
-        programmingbook.setPrice("100 руб ");
+                Book programmingbook = new Book();
+                programmingbook.setName("Лучший учебник по Джава ");
+                programmingbook.setPrice("100 руб ");
 
-        Seller ivan = new Seller();
-        ivan.setName("Иван ");
-        ivan.setWorkingDay("Вторник");
+                Seller ivan = new Seller();
+                ivan.setName("Иван ");
+                ivan.setWorkingDay("Вторник");
 
-        BasisOfCheck.showBasisOfCheck();
+                BasisOfCheck.showBasisOfCheck();
 
-        ShoppingPipeline.showSell(programmingbook, ivan);
-    }
-
+                ShoppingPipeline.showSell(programmingbook, ivan);
+        }
 }
--- src/main/java/ShoppingPipeline.java
+++ src/main/java/ShoppingPipeline.java
@@ -1,7 +1,9 @@
-public class ShoppingPipeline {
+public class ShoppingPipeline extends BasisOfCheck{
 
     public static void showSell(Book book, Seller seller) {
         System.out.println("Книгу " + book.getName() + book.getPrice() + "продал " + seller.getName() + seller.getWorkingDay());
+
+        showBasisOfCheck();
     }
 
 }
